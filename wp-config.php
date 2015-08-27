762f377436ef2597d678555af02ca62f0f0f95d4
xuant <tangbixuan@gmail.com>
2015-08-27 00:27:44 -0700

    WPDATABASE_SERVICE_HOST

--- wp-config.php
+++ wp-config.php
@@ -21,7 +21,7 @@
 $DB_NAME = getenv("DB_NAME");
 $DB_USER = getenv("DB_USER");
 $DB_PASSWORD = getenv("DB_PASSWORD");
-$DB_HOST = getenv("DB_HOST");
+$DB_HOST = getenv("WPDATABASE_SERVICE_HOST");
 
 
 // ** MySQL settings - You can get this info from your web host ** //
