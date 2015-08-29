9c1d37bbf6324a6e0b6511b4ad6576999e6442fa
xuant <tangbixuan@gmail.com>
2015-08-28 18:30:35 -0700

    commit again

--- mysql-connection.php
+++ mysql-connection.php
@@ -6,6 +6,10 @@ $DB_SERVICE = getenv("DB_SERVICE");
 
 $DB_HOST = strtoupper($DB_SERVICE) . "_SERVICE_HOST";
 
+echo $DB_HOST;
+echo $DB_USER;
+echo $DB_PASSWORD;
+
 $link = mysql_connect($DB_HOST, $DB_USER, $DB_PASSWORD);
 if (!$link) {
         die('Could not connect: ' . mysql_error());
